cec7015dadccf9f727335c7747455235e4b85697
araceli <carmona.araceli.4690@gmail.com>
2021-10-17 17:35:43 -0700

    fixed color code for hr of the day issue

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,53 @@
+
 //displaying today's date
-var currentDay = moment().format("MMM Do YYYY"); 
+var currentDay = moment().format("MMM Do YYYY");
 $('#currentDay').html(currentDay);
 
 //listening for click event on save button
-$(document).ready(function() {
-    $(".saveBtn").on("click", function() {
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         //saving items in local storage
         localStorage.setItem(time, text);
-     })
+    })
 
-     // checking current time to color code hour blocks
-     function timeCheck() {
-         var timeNow = moment().hour();
+    // checking current time to color code hour blocks
+    function timeCheck() {
+        var timeNow = moment().hour();
 
-         $(".time-block").each(function () {
-             var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hr")[1]);
 
-             // check current time against specified time blocks to color code 
+            // check current time against specified time blocks to color code 
             if (timeBlock < timeNow) {
-                 $(this).removeClass("future");
-                 $(this).removeClass("present");
-                 $(this).addClass("past");
-             }
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
             else if (timeBlock === timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("past");
                 $(this).addClass("present");
-             }
+            }
             else {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
-             }
-         })
-     }
+            }
+        })
+    }
 
-     $("#hr9 .description").val(localStorage.getItem("hr9"));
-     $("#hr10 .description").val(localStorage.getItem("hr10"));
-     $("#hr11 .description").val(localStorage.getItem("hr11"));
-     $("#hr12 .description").val(localStorage.getItem("hr12"));
-     $("#hr1 .description").val(localStorage.getItem("hr1"));
-     $("#hr2 .description").val(localStorage.getItem("hr2"));
-     $("#hr3 .description").val(localStorage.getItem("hr3"));
-     $("#hr4 .description").val(localStorage.getItem("hr4"));
-     $("#hr5 .description").val(localStorage.getItem("hr5"));
+    $("#hr8 .description").val(localStorage.getItem("hr8"));
+    $("#hr9 .description").val(localStorage.getItem("hr9"));
+    $("#hr10 .description").val(localStorage.getItem("hr10"));
+    $("#hr11 .description").val(localStorage.getItem("hr11"));
+    $("#hr12 .description").val(localStorage.getItem("hr12"));
+    $("#hr13 .description").val(localStorage.getItem("h13"));
+    $("#hr14 .description").val(localStorage.getItem("hr14"));
+    $("#hr15 .description").val(localStorage.getItem("hr15"));
+    $("#hr16 .description").val(localStorage.getItem("hr16"));
 
-     timeCheck();
+    timeCheck();
 
-    })
+})
